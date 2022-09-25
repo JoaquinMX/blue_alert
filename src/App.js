962e0cf25ce468d4b0d285d99788c0c4738d9c4d
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-09-25 01:51:05 -0500

    Create basic app bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchFolio from './SearchFolio/SearchFolio';
 import ReportGenerator from './ReportGenerator/ReportGenerator';
 import ReportCaptcha from './ReportGenerator/ReportCaptcha';
+import Dashboard from './Dashboard';
+
 function App() {
   return (
 
@@ -9,13 +11,10 @@ function App() {
       <header className='App-header'>
         <BrowserRouter>
           <Routes>
-            <Route exact path="/" element={<ReportGenerator />}>
-            </Route>
-            <Route exact path="/search" element={<SearchFolio />}>
-            </Route>
-            <Route exact path="/prueba" element={<ReportCaptcha />}>
-            </Route>
-
+            <Route exact path="/" element={<ReportGenerator />}></Route>
+            <Route exact path="/search" element={<SearchFolio />}></Route>
+            <Route exact path="/prueba" element={<ReportCaptcha />}></Route>
+            <Route exact path="/dashboard" element={<Dashboard />}></Route>
           </Routes>
       </BrowserRouter>
       </header>
