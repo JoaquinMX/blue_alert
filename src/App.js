157586b92e86458ec0d84adb6d8f15179192c79c
Joaquin Beltran <contact@joaquinmx.com>
2022-09-24 19:11:46 -0500

    [WIP] feat: Create Search Folio Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
-import ReportGenerator from "./ReportGenerator/ReportGenerator";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchFolio from './SearchFolio/SearchFolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ReportGenerator>a</ReportGenerator>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <header>Hola</header>
+      <main>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          <Route exact path="/">
+            <ReportGenerator></ReportGenerator>
+          </Route>
+          <Route path="/search" element={<SearchFolio />}>
+          </Route>
+        </Routes>
+      </main>
+      <footer>Adios</footer>
+    </BrowserRouter>
   );
 }
 
---
+++ src/SearchFolio/SearchFolio.js
@@ -0,0 +1,40 @@
+import {React, useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Container, Typography } from '@mui/material';
+import StatusCard from './StatusCard';
+
+function SearchFolio() {
+
+  const [ isItDisplayingCard, setIsItDisplayingCard ] = useState(false); 
+  const [ haveTheUserSearch, setHaveTheUserSearch] = useState(false);
+
+  useEffect( () => {
+    //REQUEST INFO TO API
+  }, [haveTheUserSearch]);
+
+  return (
+    <Container>
+        <Container>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                onKeyDown={(e) => e.key === 'Enter' ? setHaveTheUserSearch(true) : e}
+                onSubmit={(e) => e.preventDefault()}
+            >
+                <TextField id="search" label="Search" variant="outlined" />
+            </Box>
+        </Container>
+        <Container>
+            {isItDisplayingCard ? <StatusCard /> : haveTheUserSearch ? <Typography>No se han encotrado resultados con su búsqueda</Typography> : <Typography>Ingrese el folio que desea buscar</Typography>}
+        </Container>
+    </Container>
+    
+  );
+}
+
+export default SearchFolio;
