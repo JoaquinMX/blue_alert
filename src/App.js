0275660f16a9cd00bef0d5582f6e8a863e2189f8
Joaquin Beltran <contact@joaquinmx.com>
2022-09-24 19:19:59 -0500

    fix: Fix styles in App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchFolio from './SearchFolio/SearchFolio';
-
+import ReportGenerator from './ReportGenerator/ReportGenerator';
 function App() {
   return (
-    
-    <BrowserRouter>
-      <header>Hola</header>
-      <main>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Routes>
-          <Route exact path="/">
-            <ReportGenerator></ReportGenerator>
-          </Route>
-          <Route path="/search" element={<SearchFolio />}>
-          </Route>
-        </Routes>
-      </main>
-      <footer>Adios</footer>
-    </BrowserRouter>
+
+    <div className='App'>
+      <header className='App-header'>
+        <BrowserRouter>
+        <header>Hola</header>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<ReportGenerator />}>
+              
+            </Route>
+            <Route exact path="/search" element={<SearchFolio />}>
+            </Route>
+          </Routes>
+        </main>
+        <footer>Adios</footer>
+      </BrowserRouter>
+      </header>
+    </div>
   );
 }
 
